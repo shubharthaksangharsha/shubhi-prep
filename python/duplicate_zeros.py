c9e71903d38b93487955061228bbcfb665315f53
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2021-12-31 12:37:31 +0530

    Added duplicate zeros problem in python

---
+++ python/duplicate_zeros.py
@@ -0,0 +1,26 @@
+def duplicateZeros(arr):
+    """
+        Do not return anything, modify arr in-place instead.
+        """
+    n= len(arr)
+    i=0
+    #method 1
+    while i < len(arr):
+        if arr[i] == 0:
+            arr[i+1:] = arr[i:len(arr)-1]
+            i+=2
+        else:
+            i+=1
+    #method 2
+    while i < len(arr) -1:
+        if arr[i] == 0:
+            arr.insert(i+1,0)
+            del(arr[len(arr) -1])
+            i += 2
+        else:
+            i += 1
+arr = [1, 0, 2,3,0,4,5,0]
+duplicateZeros(arr)
+#output = arr  = [ 1,0,0,2,3,0,0,4,5,0,0] 
+
+
--- python/find_even.py
+++ python/find_even.py
@@ -1,16 +1,34 @@
 def findNumbers(nums):
-    count = res = 0 #initalizing the variables
-    for i in nums: #iterating the list 
-        temp_str = str(i) #converting the number into string
-        count = len(temp_str) #counting the number of digits 
-        if count % 2 == 0: 
-            res+=1 #if number of digits even then increasing the res value by 1 
-    return res #returning the answer
+    #using str.len()
+    #Time Complexity : O(N^2)
+    #Space Complexity : O(N)
+    #    count = res = 0 #initalizing the variables
+    #    for i in nums: #iterating the list
+    #        temp_str = str(i) #converting the number into string
+    #        count = len(temp_str) #counting the number of digits
+    #        if count % 2 == 0:
+    #            res+=1 #if number of digits even then increasing the res value by 1
+    #    return res #returning the answer
+
+    #Without using str.len()
+    #Time Complexity : O(N^2)
+    #Space Complexity : O(1)
+    count = res = 0  # initalizing the variables
+    for i in nums:  # iterating the list
+        while i != 0:
+            i //= 10
+            count += 1            
+        if count % 2 == 0:
+            res += 1  # if number of digits even then increasing the res value by 1
+        count = 0
+    return res  # returning the answer
+
+
 nums = []
 def input_list(nums):
     n = int(input())
     for i in range(n):
-        nums.append(int(input()))    
+        nums.append(int(input()))
 input_list(nums)
 print(nums)
 print(findNumbers(nums))
